c01fd4c61a9d966612d7232d3ad2e463a1663801
Simone R. Lubones <simone.rlubones@gmail.com>
2024-08-22 00:15:57 -0400

    dar estilo de color, corregir card, eliminar padding

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,10 @@ if(inputText != null){
 $(document).ready(function () {
 
     $('#formulario').mouseover(function(){
-        $("#formulario").css("background-color", "blue");
+        $("#formulario").css("background-color", "rgb(69, 69, 94)");
     });
     $('#formulario').mouseout(function(){
-        $("#formulario").css("background-color", "red");
+        $("#formulario").css("background-color", "rgb(101, 162, 226)");
 });
 
 $('#btnEnviar').click(function(){
