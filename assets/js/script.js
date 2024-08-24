5661aa0edb354f3db35fe67432a4bf27a70df428
Leonardo Muñoz <contacto@pctester.cl>
2024-08-23 21:04:04 -0400

    mejorando formulario & modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,4 @@
-const inputText = document.querySelector("#username");
-const labelText = document.querySelector("#basic-addon1");
 const carteleraElement = document.querySelector("#cartelera");
-
-if (inputText != null) {
-  inputText.addEventListener("keyup", (event) => {
-    console.log(event.key);
-    labelText.innerHTML += event.key;
-    if (event.key === "Backspace" || event.key === "Delete") {
-      labelText.innerHTML = "Nombre " + inputText.value;
-    }
-  });
-}
 
 const cartelera = [
   {
